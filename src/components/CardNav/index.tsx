146b257925a42a28e377a29faf93cb2d9c17fad8
Bubblesfinance <81691912+Bubblesfinance@users.noreply.github.com>
2021-06-12 21:12:05 +0300

    Update index.tsx

--- src/components/CardNav/index.tsx
+++ src/components/CardNav/index.tsx
@@ -11,7 +11,7 @@ const StyledNav = styled.div`
 const Nav = ({ activeIndex = 0 }: { activeIndex?: number }) => (
   <StyledNav>
     <ButtonMenu activeIndex={activeIndex} size="sm" variant="subtle">
-      <ButtonMenuItem id="swap-nav-link" to="/swap?outputCurrency=0xe1db3d1ee5cfe5c6333be96e6421f9bd5b85c987" as={Link}>
+      <ButtonMenuItem id="swap-nav-link" to="/swap?outputCurrency=0x272E923B510154F6285C5A38F24d448d53a833DD" as={Link}>
         <TranslatedText translationId={8}>Swap</TranslatedText>
       </ButtonMenuItem>
       <ButtonMenuItem id="pool-nav-link" to="/pool" as={Link}>
